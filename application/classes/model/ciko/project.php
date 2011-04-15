5d18552f6e1c44966810e5d1a180f9f8fd8f34c9
Jeremy Bush <contractfrombelow@gmail.com>
2011-04-15 17:02:27 -0500

    adding basic project model and config file

---
+++ application/classes/model/ciko/project.php
@@ -0,0 +1,90 @@
+<?php
+
+class Model_Ciko_Project extends Model
+{
+	// The project name
+	protected $_name;
+
+	// The repository to build
+	protected $_repository;
+
+	// The branch to build
+	protected $_branch;
+
+	// The runner command to execute
+	protected $_runner;
+
+	/**
+	 * Constructor that sets the project name
+	 *
+	 * @return null
+	 */
+	public function __construct($name)
+	{
+		$this->name($name);
+	}
+
+	/**
+	 * Gets/sets the name of this project
+	 *
+	 * @return $this or string
+	 */
+	public function name($name = NULL)
+	{
+		if ($name)
+		{
+			$this->_name = $name;
+			return $this;
+		}
+
+		return $this->_name;
+	}
+
+	/**
+	 * Gets/sets the repository for this project
+	 *
+	 * @return $this or string
+	 */
+	public function repository($repo = NULL)
+	{
+		if ($repo)
+		{
+			$this->_repository = $repo;
+			return $this;
+		}
+
+		return $this->_repository;
+	}
+
+	/**
+	 * Gets/sets the branch for this project
+	 *
+	 * @return $this or string
+	 */
+	public function branch($branch = NULL)
+	{
+		if ($branch)
+		{
+			$this->_branch = $branch;
+			return $this;
+		}
+
+		return $this->_branch;
+	}
+
+	/**
+	 * Gets/sets the runner command for this project
+	 *
+	 * @return $this or string
+	 */
+	public function runner($command = NULL)
+	{
+		if ($command)
+		{
+			$this->_runner = $command;
+			return $this;
+		}
+
+		return $this->_runner;
+	}
+}
---
+++ application/config/ciko.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Config file for Ciko. Specify your projects here in this format:
+ * 
+ * 	$project = new Model_Ciko_Project('Ciko');
+ * 	$project->repository('git://github.com/zombor/Ciko.git');
+ * 	$project->branch('master');
+ * 	$project->runner('phpunit');
+ * 	
+ * 	$projects['ciko'] = $project;
+ *
+ * @package    Ciko
+ * @author     Jeremy Bush
+ * @copyright  (c) 2010 Jeremy Bush
+ * @license    http://github.com/zombor/Ciko/raw/develop/LICENSE
+ */
+
+$projects = array();
+
+
+return $projects;
