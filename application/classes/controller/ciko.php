57cee06f059951ec75c89f940e58b1304fb89f58
Jeremy Bush <contractfrombelow@gmail.com>
2011-04-17 17:14:28 -0500

    Adding controller skeleton

---
+++ application/classes/controller/ciko.php
@@ -0,0 +1,42 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Main Ciko controller, displays project runner results and schedules runners
+ *
+ * @package    Ciko
+ * @author     Jeremy Bush
+ * @copyright  (c) 2010 Jeremy Bush
+ * @license    http://github.com/zombor/Ciko/raw/develop/LICENSE
+ */
+class Controller_Ciko extends Controller
+{
+	/**
+	 * Shows all projects and their latest status
+	 *
+	 * @return null
+	 */
+	public function action_index()
+	{
+		
+	}
+
+	/**
+	 * Shows all runner jobs for a project
+	 *
+	 * @return null
+	 */
+	public function action_project($project = NULL)
+	{
+		
+	}
+
+	/**
+	 * Schedules a runner job for a project
+	 *
+	 * @return null
+	 */
+	public function action_run($project)
+	{
+		
+	}
+}
