bb78c6fa49835139e5b007697da41331f3c455b5
Jeremy Bush <contractfrombelow@gmail.com>
2011-04-18 07:26:03 -0500

    Add error handling to ciko minion task

--- application/classes/minion/task/ciko/run.php
+++ application/classes/minion/task/ciko/run.php
@@ -41,62 +41,79 @@ class Minion_Task_Ciko_Run extends Minion_Task
 			return Minion_CLI::write('That project doesn\'t exist!', 'red');
 		}
 
-		// Clone the source
-		Git::$git_path = trim(`which git`);
-		$git = new Git(
-			Kohana::config('ciko.clone_path').url::title($project->name())
-		);
-		Minion_CLI::write(
-			$git->clone_remote(
-				$project->repository(), '-b '.$project->branch().' --recursive',
-				TRUE
-			),
-			'yellow'
-		);
-
-		$runner = Runner::factory($project->runner(), Kohana::config('ciko.clone_path').url::title($project->name()));
-
-		$status = $runner->execute();
-
-		// Run reporters if status was success
-		if ($status)
+		try
 		{
-			$output = array(
-				'stdout' => $runner->stdout(),
-				'reporters' => array(),
+			// Clone the source
+			Git::$git_path = trim(`which git`);
+			$git = new Git(
+				Kohana::config('ciko.clone_path').url::title($project->name())
+			);
+			Minion_CLI::write(
+				$git->clone_remote(
+					$project->repository(), '-b '.$project->branch().
+					' --recursive',
+					TRUE
+				),
+				'yellow'
 			);
 
-			foreach ($project->reporters() as $reporter)
+			$runner = Runner::factory(
+				$project->runner(),
+				Kohana::config('ciko.clone_path').url::title($project->name())
+			);
+
+			$status = $runner->execute();
+
+			// Run reporters if status was success
+			if ($status)
 			{
-				$output['reporters'][$reporter->name()] = $reporter->analyze();
+				$output = array(
+					'stdout' => $runner->stdout(),
+					'reporters' => array(),
+				);
+
+				foreach ($project->reporters() as $reporter)
+				{
+					$output['reporters'][$reporter->name()] =
+						$reporter->analyze();
+				}
+
+				$project->write(
+					json_encode($output), $runner->stderr(), $status
+				);
+			}
+			else
+			{
+				$project->write(
+					json_encode($stdout), $runner->stderr(), $status
+				);
 			}
 
-			$project->write(json_encode($output), $runner->stderr(), $status);
-		}
-		else
-		{
-			$project->write(json_encode($stdout), $runner->stderr(), $status);
-		}
+			// Run notifiers
+			foreach ($project->notifiers() as $notifier)
+			{
+				$notifier->execute($project);
+			}
 
-		// Run notifiers
-		foreach ($project->notifiers() as $notifier)
-		{
-			$notifier->execute($project);
+			if ($status)
+			{
+				return Minion_CLI::write(
+					'Job successfully ran for project '.$config['project'].':'
+						.PHP_EOL.$runner->stdout(),
+					'green');
+			}
+			else
+			{
+				return Minion_CLI::write(
+					'There was an error executing the runner ('.$status.'):'
+						.PHP_EOL.$runner->stderr().')',
+					'red');
+			}
 		}
-
-		if ($status)
+		catch (Exception $e)
 		{
-			return Minion_CLI::write(
-				'Job successfully ran for project '.$config['project'].':'
-					.PHP_EOL.$runner->stdout(),
-				'green');
-		}
-		else
-		{
-			return Minion_CLI::write(
-				'There was an error executing the runner ('.$status.'):'
-					.PHP_EOL.$runner->stderr().')',
-				'red');
+			// Write out the error
+			$project->write('', json_encode($e->getMessage()), $e->getCode());
 		}
 	}
 }
